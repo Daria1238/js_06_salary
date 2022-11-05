74bb74636ee0118cd3b9ba671dbe556341103705
daria <daria.ruchkina4@gmail.com>
2022-11-06 00:16:53 +0200

    homework6

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function byModule(props) {
-	// write your code here
+	const totalBeefPrice = props.saltBeefBought * props.saltBeefPrice;
+	const totalRicePrice = props.redRiceBought * props.redRicePrice;
+	const totalVinePrice = props.oldVineBought * props.oldVinePrice;
+	const remainder = props.salary - totalBeefPrice - totalRicePrice - totalVinePrice;
+	const totalRemainder = remainder % props.exchange;
+
+	return totalRemainder;
 }
 
 module.exports = byModule;
